c5723d3429dee6425f77f7d33a045258b2af215f
Precious Opeyemi <precious.opeyemi@miva.edu.ng>
2025-11-12 16:05:08 +0100

    fix: Update S3 backend region to us-east-1 in Terraform configuration

--- backend/main.tf
+++ backend/main.tf
@@ -17,7 +17,7 @@ terraform {
   backend "s3" {
     bucket = "synoptik-terraform-state"
     key    = "terraform.tfstate"
-    region = var.region
+    region = "us-east-1"
     encrypt = true
     dynamodb_table = "synoptik-terraform-state-lock"
   }
