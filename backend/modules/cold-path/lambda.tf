12c83ab75f4774ab25c07571f141de29b5590c05
Precious Opeyemi <precious.opeyemi@miva.edu.ng>
2025-12-02 12:54:49 +0100

    feat: add crawler ARN to lambda policy resources

--- backend/modules/cold-path/lambda.tf
+++ backend/modules/cold-path/lambda.tf
@@ -230,6 +230,7 @@ data "aws_iam_policy_document" "telemetry_aggregator_lambda_policy" {
     ]
     resources = [
       aws_dynamodb_table.telemetry.arn,
+      aws_crawler.crawler.arn,
       "${aws_dynamodb_table.telemetry.arn}/index/*"
     ]
   }
